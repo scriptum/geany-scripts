bc3c9448d47e0fc2b715a5aadaf9500e0fe5556d
Pavel Roschin <roshin@scriptumplus.ru>
2014-05-05 11:15:21 +0400

    Fix for function pointers

--- align-declarations.py
+++ align-declarations.py
@@ -12,7 +12,7 @@ for line in fileinput.input():
 	m = re.search(exp, line)
 	other = m.group(3)
 	word = m.group(2)
-	curlen = len(word) + len(other) - len(other.lstrip("*"))
+	curlen = len(word) + len(other) - len(other.lstrip("*("))
 	if curlen > maxlen:
 		maxlen = curlen
 	lines.append([m.group(1), m.group(2), m.group(3)])
