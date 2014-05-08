85772bb6ae4bb3c397151f6a5047629b81954f43
Pavel Roschin <roshin@scriptumplus.ru>
2014-05-08 18:19:21 +0400

    add more chars to ignore

--- align-declarations.py
+++ align-declarations.py
@@ -6,14 +6,16 @@ import sys, fileinput, re
 lines = []
 maxlen = 0
 
-exp = re.compile("(^\s*)((?:static |const )*\w+)\s*(.*)$")
+exp = re.compile("(^\s*)((?:static |const |#define )*\w+)\s*(.*)$")
+
+STRIPCHARS="*()-"
 
 for line in fileinput.input():
 	m = re.search(exp, line)
 	if m:
 		other = m.group(3)
 		word = m.group(2)
-		curlen = len(word) + len(other) - len(other.lstrip("*("))
+		curlen = len(word) + len(other) - len(other.lstrip(STRIPCHARS))
 		if curlen > maxlen:
 			maxlen = curlen
 		lines.append([m.group(1), m.group(2), m.group(3)])
@@ -24,6 +26,6 @@ maxlen += 1
 
 for line in lines:
 	if type(line) != "str" and len(line) == 3:
-		print ("%s%-"+str(maxlen - len(line[2]) + len(line[2].lstrip("*(")))+"s%s") % (line[0], line[1], line[2])
+		print ("%s%-"+str(maxlen - len(line[2]) + len(line[2].lstrip(STRIPCHARS)))+"s%s") % (line[0], line[1], line[2])
 	else:
 		print line.rstrip("\n\r")
